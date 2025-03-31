e3663c340201135b7eced865d33473cc5b943a3b
TwoCreepers <z15296529345@outlook.com>
2025-03-31 18:12:27 +0800

    更新py接口

--- python_interface/main.cpp
+++ python_interface/main.cpp
@@ -4,6 +4,7 @@
 #include "register_database.hpp"
 #include "register_exceptions.hpp"
 #include "register_sqlite_checkpoint.hpp"
+#include "register_synchronous_mode.hpp"
 #include "resister_table.hpp"
 #include <py.hpp>
 #include <pybind11/detail/common.h>
@@ -12,10 +13,11 @@ namespace py = pybind11;
 namespace me = memory;
 PYBIND11_MODULE(qbot_memory, m)
 {
+	register_sqlite_checkpoint(m);
+	register_sqlite_synchronous_mode(m);
 	register_exceptions(m);
 	register_ckecks(m);
 	register_data(m);
-	register_sqlite_checkpoint(m);
 	register_database(m);
 	register_table(m);
 }
